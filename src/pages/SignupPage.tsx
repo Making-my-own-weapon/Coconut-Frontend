4702fa88bda9178192483b4c9466e5d9a6549989
이의재 <luj000616@gmail.com>
2025-07-01 05:11:42 +0900

    feat(#23): signup page 구현

---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MainBackground from '../components/main/MainBackground'; // 1. 레이아웃 틀을 가져옵니다.
+import SigninCard from '../components/signup/SignupCard'; // 2. 내용물을 가져옵니다.
+
+const SigninPage: React.FC = () => {
+  return (
+    // 3. MainBackground라는 '틀' 안에,
+    <MainBackground>
+      {/* 4. HeroSection이라는 '내용물'을 쏙 넣어줍니다. */}
+      <SigninCard />
+    </MainBackground>
+  );
+};
+
+export default SigninPage;
