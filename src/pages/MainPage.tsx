3565367b823421e0b49bf600aac45054aea8c6fe
이의재 <luj000616@gmail.com>
2025-07-01 04:12:50 +0900

    feat(#17):mainpage 구현

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import MainBackground from '../components/main/MainBackground'; // 1. 레이아웃 틀을 가져옵니다.
+import HeroSection from '../components/main/HeroSection'; // 2. 내용물을 가져옵니다.
+import FeatureCards from '../components/main/FeatureCards'; // FeatureCards 추가
+
+const MainPage: React.FC = () => {
+  return (
+    // 3. MainBackground라는 '틀' 안에,
+    <MainBackground>
+      <div className="flex flex-col min-h-screen justify-start items-center w-full px-2 sm:px-0 pt-0 sm:pt-8 lg:pt-16">
+        <HeroSection />
+        <FeatureCards />
+      </div>
+    </MainBackground>
+  );
+};
+
+export default MainPage;
