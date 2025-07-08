304d794dfdd5e86b72ef820980842496643c5269
BJJ <a01072004146@gmail.com>
2025-07-08 14:47:14 +0900

    Chore(#62): .env를 통한 url 진입

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001', {
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+const socket = io(SOCKET_URL, {
   autoConnect: false,
 });
 
